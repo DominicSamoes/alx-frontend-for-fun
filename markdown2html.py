c6a5b843159ad6ab2f3787f3e367f2d123157835
DominicSamoes <dominicsamo@gmail.com>
2023-01-16 10:15:24 +0200

    Task 0

---
+++ markdown2html.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Script changes Markdown to HTML
+"""
+
+if __name__ == "__main__":
+    import sys
+    from os import path
+    import re
+    import hashlib
+
+    if len(sys.argv) < 2:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        exit(1)
+
+    if not path.exists(sys.argv[1]):
+        sys.stderr.write("Missing " + sys.argv[1] + '\n')
+        exit(1)
